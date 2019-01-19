af6d452ce343b2a06e41075960b5821de266d194
Erick Henriquez <henriquez93550@yahoo.com>
2019-01-19 11:43:07 -0600

    working on onClick events

--- src/components/FriendCard/index.js
+++ src/components/FriendCard/index.js
@@ -1,29 +1,58 @@
-import React from "react";
+import React, { Component } from 'react';
 import "./style.css";
 
-function FriendCard(props) {
-  return (
-    <div className="card">
-      <div className="img-container">
-        <img alt={props.name} src={props.image} />
-      </div>
-      <div className="content">
-        <ul>
-          <li>
-            <strong>Name:</strong> {props.name}
-          </li>
-          <li>
-            <strong>Occupation:</strong> {props.occupation}
-          </li>
-          <li>
-            <strong>Location:</strong> {props.location}
-          </li>
-        </ul>
-      </div>
-      <span onClick={() => props.removeFriend(props.id)} className="remove">
-      </span>
-    </div>
-  );
+class FriendCard extends Component {
+  constructor(props) {
+    super(props);
+  this.state = {
+    clicked: false
+  };
+}
+
+ // when you click on an image
+    handleClickTrue = (event, clicked) => {
+    if(clicked === true) {// game is now over
+      // update the display
+      this.props.changeDisplayLoser();
+      // reset the score
+      this.props.resetScore();
+    } else { 
+     // update the display
+      this.props.changeDisplayCorrect();
+      // update the status of "clicked" to true
+      this.props.correctGuess(event);
+      // shuffle the images around
+      this.props.shuffle();
+      // increase the score by one
+      this.props.setScore();
+     
+    }
+    }; 
+    
+    
+    render() {
+        return (
+          <div className="card">
+          <div className="img-container">
+            <img alt={this.props.id} src={this.props.image} onClick={() => this.handleClickTrue(this.props.id, this.props.clicked)} />
+          </div>
+          <div className="content">
+            <ul>
+              <li>
+                <strong>Name:</strong> {this.props.name}
+              </li>
+              <li>
+                <strong>Occupation:</strong> {this.props.occupation}
+              </li>
+              <li>
+                <strong>Location:</strong> {this.props.location}
+              </li>
+            </ul>
+          </div>
+        </div>
+            
+            );
+    }
 }
 
 export default FriendCard;
