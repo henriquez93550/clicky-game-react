369d12da3e8c89b5a8c27793551a4ac9647da005
Erick Henriquez <henriquez93550@yahoo.com>
2019-01-19 13:21:04 -0600

    Updates

---
+++ precache-manifest.a9e08b964cd7e35d8b78dbf5ec777b90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7396b556b27b2650d61",
+    "url": "/clicky-game-react/static/css/main.9df25874.chunk.css"
+  },
+  {
+    "revision": "c7396b556b27b2650d61",
+    "url": "/clicky-game-react/static/js/main.c7396b55.chunk.js"
+  },
+  {
+    "revision": "816c52d3695556406acc",
+    "url": "/clicky-game-react/static/js/1.816c52d3.chunk.js"
+  },
+  {
+    "revision": "1ffa1f4187ecf54ec1d4",
+    "url": "/clicky-game-react/static/js/runtime~main.1ffa1f41.js"
+  },
+  {
+    "revision": "695ad1e23b3676a67057b50c7282e3ab",
+    "url": "/clicky-game-react/index.html"
+  }
+];
