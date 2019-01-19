8345e0ac8ef6c090e274549aa3f78049973b8b8f
Erick Henriquez <henriquez93550@yahoo.com>
2019-01-19 12:53:24 -0600

    functional not pretty

--- src/App.js
+++ src/App.js
@@ -7,111 +7,96 @@ import friends from "./friends.json";
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
-      score: 0,
-      topScore: 0,
-      friends: friends,
-      display: "Click A Tile To Start",
-      clickedFriends: []
+    score: 0,
+    topScore: 0,
+    friends: friends,
+    display: "Click A Tile To Start",
+    clickedFriends: []
   };
 
-   // function to show user got answer correctly
-   changeDisplayCorrect = () => {
-    // update the display 
-    this.setState({display: "Correct!"});
-      }
+  // function to show user got answer correctly
+  changeDisplayCorrect = () => {
+    // update the display
+    this.setState({ display: "Correct!" });
+  };
 
-    // display this if they lost  
-    changeDisplayLost = () => {
-      // update the display 
-      this.setState({display: "Game Over! Try Again!"});
-     }
+  // display this if they lost
+  changeDisplayLost = () => {
+    // update the display
+    this.setState({ display: "Game Over! Try Again!" });
+  };
 
-  // removeFriend = id => {
-  //   // Filter this.state.friends for friends with an id not equal to the id being removed
-  //   const friends = this.state.friends.filter(friend => friend.id !== id);
-  //   // Set this.state.friends equal to the new friends array
-  //   this.setState({ friends });
-  // };
-  setScore = () => {  // increase score by one
+  setScore = () => {
+    // increase score by one
     this.setState({ score: this.state.score + 1 });
-    // return this.state.score;
-    //console.log(this.state.score);
   };
-  resetScore = () => { // update the topScore if the score is > topScore
+
+  resetScore = () => {
+    // update the topScore if the score is > topScore
     if (this.state.score > this.state.topScore) {
-        this.setState({ topScore: this.state.score});
-        this.setState({ score: this.state.score = 0});// then reset score to 0
-        this.resetFriends() 
-     }else {// reset the score
-       this.setState({ score: this.state.score = 0});
-         // reset the clicked value of all the cards
-        this.setState({clickedFriends: []});
-        this.resetFriends()
-        
-   }
- }
- resetFriends = () => {
-  const sorted = [...this.state.friends]
-
-  for (var i = 0; i < sorted.length; i += 1) {
-    delete sorted[i].clicked
-    sorted[i].clicked = false
-  }
-  console.log("+++++++++++++++++++++++++++++" + typeof sorted)
-
-  this.setState({friends: sorted}, function() {
-    console.log(this.state.friends)
-    console.log("--------------------------------------------------------" + JSON.stringify(this.state))
-  })
-}  
-
-handleShuffle = () => { // copy all the values from this.state.friends... 
-  // and puts them into this array
-    const sorted = [...this.state.friends]
-        
-      // shuffle the images around
-    sorted.sort(function(a, b){return 0.5 - Math.random()});
-    this.setState({ // replace the friends array with the sorted array 
-        friends: sorted
-    }) 
+      this.setState({ topScore: this.state.score });
+      this.setState({ score: (this.state.score = 0) }); // then reset score to 0
+      this.resetFriends();
+    } else {
+      // reset the score
+      this.setState({ score: (this.state.score = 0) });
+      // reset the clicked value of all the cards
+      this.setState({ clickedFriends: [] });
+      this.resetFriends();
     }
-correctGuess = (event) => {
-  const sorted = {...this.state.friends}
-  console.log(sorted[0])
-  console.log("guessed ID " + event)
-  
-  for (var i = 0; i < this.state.friends.length; i += 1) {
+  };
 
-    if (sorted[i].id === event) {
-      delete sorted[i].clicked
-      console.log("Found")
-      sorted[i].clicked = true
-      console.log("clicked " + sorted[i].clicked)
-    }   else {
-      console.log("not working")
+  resetFriends = () => {
+    const sorted = [...this.state.friends];
+
+    for (var i = 0; i < sorted.length; i += 1) {
+      delete sorted[i].clicked;
+      sorted[i].clicked = false;
     }
-  }
-  this.setState({friends: sorted}, function() {
-    console.log(this.state.friends)
-  })
+    this.setState({ friends: sorted }, function() {});
+  };
 
-  
-}
+  handleShuffle = () => {
+    // copy all the values from this.state.friends...
+    // and puts them into this array
+    const sorted = [...this.state.friends];
+
+    // shuffle the images around
+    sorted.sort(function(a, b) {
+      return 0.5 - Math.random();
+    });
+    this.setState({
+      // replace the friends array with the sorted array
+      friends: sorted
+    });
+  };
+
+  correctGuess = event => {
+    const sorted = { ...this.state.friends };
+    for (var i = 0; i < this.state.friends.length; i += 1) {
+      if (sorted[i].id === event) {
+        delete sorted[i].clicked;
+        sorted[i].clicked = true;
+      } else {
+      }
+    }
+    this.setState({ friends: sorted }, function() {});
+  };
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <Wrapper>
-        <Title 
-            score={this.state.score}
-            topScore={this.state.topScore}
-            display={this.state.display}
-            />
+        <Title
+          score={this.state.score}
+          topScore={this.state.topScore}
+          display={this.state.display}
+        />
         {/* for every friend in the friend card return data */}
         {this.state.friends.map(friend => (
           <FriendCard
             resetScore={this.resetScore}
             changeDisplayCorrect={this.changeDisplayCorrect}
-            changeDisplayLoser={this.changeDisplayLoser}
+            changeDisplayLoser={this.changeDisplayLost}
             handleClickTrue={this.handleClickTrue}
             setScore={this.setScore}
             id={friend.id}
@@ -120,6 +105,9 @@ correctGuess = (event) => {
             image={friend.image}
             occupation={friend.occupation}
             location={friend.location}
+            shuffle={this.handleShuffle}
+            clicked={friend.clicked}
+            correctGuess={this.correctGuess}
           />
         ))}
       </Wrapper>
